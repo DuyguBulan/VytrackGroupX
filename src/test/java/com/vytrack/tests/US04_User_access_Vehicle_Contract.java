b6d9f8879cc4f73f4dffaa79a0997030d3aa86a5
ozturkkerem <ozturk.kerem@live.com>
2022-03-13 21:53:50 -0400

    US04_ Done

--- src/test/java/com/vytrack/tests/US04_User_access_Vehicle_Contract.java
+++ src/test/java/com/vytrack/tests/US04_User_access_Vehicle_Contract.java
@@ -5,6 +5,8 @@ import com.vytrack.tests.base.TestBase;
 import com.vytrack.utilities.BrowserUtils;
 import com.vytrack.utilities.ConfigurationReader;
 import com.vytrack.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -40,7 +42,7 @@ public class US04_User_access_Vehicle_Contract extends TestBase {
     }
 
     @Test
-    public void Store_managers_access_the_vehicle_contract_page(){
+    public void store_managers_access_the_vehicle_contract_page(){
         // Go to the login page
         loginPage.gotoLoginPage();
 
@@ -65,6 +67,35 @@ public class US04_User_access_Vehicle_Contract extends TestBase {
         String actualTitle = Driver.getDriver().getTitle();
 
         Assert.assertEquals(expectedTitle,actualTitle);
+    }
+
+    @Test
+    public void driver_access_the_vehicle_contract_page(){
+        // Go to the login page
+        loginPage.gotoLoginPage();
+
+        //User login with sales manager user and password
+        loginPage.login(ConfigurationReader.getProperty("driver_username"), ConfigurationReader.getProperty("driver_password"));
+
+        BrowserUtils.waituntilTitleDisplay("Dashboard");
+
+
+        //Hover to Fleet Module
+        BrowserUtils.sleep(2);
+        BrowserUtils.hover(homePage.fleetModule);
+
+        //Hover to subModule Driver contract
+        BrowserUtils.hover(homePage.vehicle_contracts_submodule);
+
+        //Click Driver contract
+        homePage.vehicle_contracts_submodule.click();
+        BrowserUtils.sleep(2);
+
+        //Compare expected and actual result
+        String expectedNote = "You do not have permission to perform this action.";
+        String actualTitle = Driver.getDriver().findElement(By.xpath("//div[@class='message']")).getText();
+
+        Assert.assertEquals(expectedNote,actualTitle);
     }
 
 
